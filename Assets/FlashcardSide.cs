8b1e6d0f61c812de5665032d3f92348c9e1d7893
ll2585 <ll2585@columbia.edu>
2019-10-29 14:44:09 -0400

    changes

---
+++ Assets/FlashcardSide.cs
@@ -0,0 +1,15 @@
+﻿    public class FlashcardSide
+    {
+        public string value { get;  }
+        public string additional_value { get; }
+        public string value_3 { get; }
+
+        public FlashcardSide(string value, string additionalValue = null, string value3 = null)
+        {
+            this.value = value;
+            this.additional_value = additionalValue;
+            this.value_3 = value3;
+        }
+        
+        
+    }
